91500a58a9d2280bf916d9f1746313ea40b084fe
vladbalaneanu <vlad.bala17@gmail.com>
2024-02-02 20:09:36 +0200

    created SettingsScreen fields

--- app/src/main/java/com/vlad/bikegarage/settings/presentation/SettingsScreen.kt
+++ app/src/main/java/com/vlad/bikegarage/settings/presentation/SettingsScreen.kt
@@ -1,4 +1,7 @@
-@file:OptIn(ExperimentalMaterialApi::class)
+@file:OptIn(
+    ExperimentalMaterialApi::class, ExperimentalMaterialApi::class,
+    ExperimentalMaterialApi::class
+)
 
 package com.vlad.bikegarage.settings.presentation
 
@@ -8,43 +11,91 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ExposedDropdownMenuBox
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vlad.bikegarage.R
 import com.vlad.bikegarage.ui.theme.BikeGarageTheme
+import com.vlad.bikegarage.util.SuffixTransformation
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SettingsScreen() {
-    Column(modifier = Modifier.fillMaxSize().background(colorResource(R.color.deep_dark_blue)).padding(16.dp)) {
-           Label(stringResource(R.string.distance_units_label), true)
-           DropDownField(listOf("KM", "Miles"), R.drawable.icon_dropdown, modifier = Modifier.fillMaxWidth())
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(R.color.deep_dark_blue))
+            .padding(16.dp)
+    ) {
+        Label(
+            stringResource(R.string.distance_units_label),
+            true,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        DropDownField(
+            listOf(
+                stringResource(id = R.string.distance_unit_km),
+                stringResource(id = R.string.distance_unit_miles)
+            ),
+            R.drawable.icon_dropdown,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Label(
+            stringResource(R.string.service_reminder_label),
+            false,
+            modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
+        )
+        Row(modifier = Modifier.fillMaxWidth()) {
+            NumericTextField(modifier = Modifier.weight(1f))
+            DefaultSwitch(modifier = Modifier.align(Alignment.CenterVertically))
+        }
+        Label(
+            stringResource(R.string.default_bike_label),
+            true,
+            modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
+        )
+        DropDownField(
+            listOf(
+                "Bike 1",
+                "Bike 2",
+                "Bike 3",
+                "Bike 4",
+                "Bike 5",
+            ),
+            R.drawable.icon_dropdown,
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
 
 @Composable
 fun Label(title: String = "", isMandatory: Boolean = false, modifier: Modifier = Modifier) {
-    Row {
+    Row(modifier = modifier) {
         Text(
             text = title,
             style = MaterialTheme.typography.h1,
-            color = colorResource(R.color.gray)
+            color = colorResource(R.color.gray),
         )
         if (isMandatory) {
             Icon(
@@ -75,13 +126,20 @@ fun DropDownField(listItems: List<String>, icon: Int, modifier: Modifier = Modif
         }
     ) {
         TextField(
-            modifier = modifier,
             value = selectedItem,
             onValueChange = {},
             readOnly = true,
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = Color.White,
+            ),
             trailingIcon = {
-                painterResource(icon)
-            }
+                Icon(
+                    painter = painterResource(id = icon),
+                    contentDescription = null,
+                    tint = Color.White
+                )
+            },
+            modifier = modifier
         )
 
         ExposedDropdownMenu(
@@ -97,7 +155,10 @@ fun DropDownField(listItems: List<String>, icon: Int, modifier: Modifier = Modif
                         expanded = false
                     }
                 ) {
-                    Text(text = selectedOption)
+                    Text(
+                        text = selectedOption,
+                        color = Color.White
+                    )
                 }
             }
         }
@@ -105,15 +166,54 @@ fun DropDownField(listItems: List<String>, icon: Int, modifier: Modifier = Modif
     }
 }
 
+@Composable
+fun NumericTextField(modifier: Modifier = Modifier) {
+    var selectedItem by remember {
+        mutableStateOf("100")
+    }
+
+    Row(
+        modifier = modifier
+    ) {
+        TextField(
+
+            value = "$selectedItem",
+            onValueChange = {
+                selectedItem = it
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            ),
+            singleLine = true,
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = Color.White,
+            ),
+            visualTransformation = SuffixTransformation(" km"),
+            modifier = modifier
+        )
+    }
+}
+
+@Composable
+fun DefaultSwitch(modifier: Modifier) {
+    var checked by remember {
+        mutableStateOf(true)
+    }
+
+    Switch(checked = checked, onCheckedChange = {
+        checked = it
+    }, modifier = modifier)
+}
+
+@ExperimentalMaterialApi
 @Preview(showBackground = true)
 @Composable
 fun Development() {
     BikeGarageTheme {
-        val dropdownList = listOf("KM", "Miles")
-
-        DropDownField(dropdownList, R.drawable.icon_dropdown)
+        NumericTextField(Modifier.fillMaxWidth())
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
---
+++ app/src/main/java/com/vlad/bikegarage/util/SuffixTransformation.kt
@@ -0,0 +1,27 @@
+package com.vlad.bikegarage.util
+
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
+
+class SuffixTransformation(private val suffix: String) : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+
+        val result = text + AnnotatedString(suffix)
+
+        val textWithSuffixMapping = object : OffsetMapping {
+            override fun originalToTransformed(offset: Int): Int {
+                return offset
+            }
+
+            override fun transformedToOriginal(offset: Int): Int {
+                if (text.isEmpty()) return 0
+                if (offset >= text.length) return text.length
+                return offset
+            }
+        }
+
+        return TransformedText(result, textWithSuffixMapping)
+    }
+}
