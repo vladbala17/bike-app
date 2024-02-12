853b31344a8a3bec387745c5c004433526eabd24
vladbalaneanu <vlad.bala17@gmail.com>
2024-02-12 18:32:28 +0200

    added empty layout for bikes screen

--- app/src/main/java/com/vlad/bikegarage/bikes/presentation/BikesScreen.kt
+++ app/src/main/java/com/vlad/bikegarage/bikes/presentation/BikesScreen.kt
@@ -1,12 +1,20 @@
 package com.vlad.bikegarage.bikes.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,23 +23,64 @@ import com.vlad.bikegarage.bikes.presentation.components.ActionButton
 
 @Composable
 fun BikesScreen() {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(all = 8.dp),
     ) {
-        ActionButton(
-            text = stringResource(R.string.add_bike_label),
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .fillMaxWidth()
-        )
+        LazyColumn(
+        ) {
+            item {
+                EmptyHeader()
+            }
+        }
     }
-
 }
 
 @Preview
 @Composable
 fun BikeScreenPreview() {
     BikesScreen()
+}
+
+@Preview
+@Composable
+fun EmptyHeader(
+    pageTitle: String = "Bikes", icon: Int = R.drawable.missing_bike_card,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = icon),
+            contentDescription = "bike",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.heightIn(0.dp, 200.dp)
+        )
+        Box(
+            modifier = Modifier
+                .align(Alignment.Start)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.dotted_line),
+                contentDescription = "dotted line",
+                modifier = Modifier
+                    .padding(start = 20.dp)
+            )
+            Text(
+                text = "You don't have any bike added yet.\n Add your first bike and start register \n your" +
+                        "rides in order to jeep your bike \n service cycles up to date.",
+                color = Color.White,
+                modifier = Modifier.align(Alignment.Center),
+            )
+            ActionButton(
+                text = stringResource(R.string.add_bike_label),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+            )
+        }
+
+    }
 }
